1628256d6a9094d5bef4bdfeda2296f8f5ae5e99
sumanta2 <sumantagorai.21@gmail.com>
2022-05-14 18:09:48 +0530

    2 add 2 line

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,9 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <div className={styles.main}>
     <h3>Welcome to my first development commit</h3>
+    <h4>This is may next line</h4>
+    </div>
   )
 }
